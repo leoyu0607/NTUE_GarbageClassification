b47e8426c27f19ef80f897d1e04ad8b0b0097b0d
LeoYu <a18600083@gmail.com>
2023-09-19 14:14:10 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,127 @@
+import os
+import torch
+import torchvision
+from torch.utils.data import random_split
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+
+data_dir = 'C:/Users/leozh/PycharmProjects/NTUE_Garbage_Classification/dataset'
+classes = os.listdir(data_dir)
+print(classes)
+
+transformations = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+dataset = ImageFolder(data_dir, transform=transformations)
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+def show_sample(img,label):
+  print("label:",dataset.classes[label],"Class No:"+ str(label) + ")")
+  plt.imshow(img.permute(1,2,0))
+
+  def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+class ImageClassificationBase(nn.Module):
+  def training_step(self, batch):
+    images, labels = batch
+    out = self(images)  # Generate predictions
+    loss = F.cross_entropy(out, labels)  # Calculate loss
+    return loss
+
+  def validation_step(self, batch):
+    images, labels = batch
+    out = self(images)  # Generate predictions
+    loss = F.cross_entropy(out, labels)  # Calculate loss
+    acc = accuracy(out, labels)  # Calculate accuracy
+    return {'val_loss': loss.detach(), 'val_acc': acc}
+
+  def validation_epoch_end(self, outputs):
+    batch_losses = [x['val_loss'] for x in outputs]
+    epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+    batch_accs = [x['val_acc'] for x in outputs]
+    epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+    return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+  def epoch_end(self, epoch, result):
+    print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+      epoch + 1, result['train_loss'], result['val_loss'], result['val_acc']))
+
+#
+
+  def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+      return torch.device('cuda')
+    else:
+      return torch.device('cpu')
+
+
+def to_device(data, device):
+  """Move tensor(s) to chosen device"""
+  if isinstance(data, (list, tuple)):
+    return [to_device(x, device) for x in data]
+  return data.to(device, non_blocking=True)
+
+
+class DeviceDataLoader():
+  """Wrap a dataloader to move data to a device"""
+
+  def __init__(self, dl, device):
+    self.dl = dl
+    self.device = device
+
+  def __iter__(self):
+    """Yield a batch of data after moving it to device"""
+    for b in self.dl:
+      yield to_device(b, self.device)
+
+  def __len__(self):
+    """Number of batches"""
+    return len(self.dl)
+
+#
+
+device = get_default_device()
+device
+
+#
+
+def predict_image(img,model):
+  # Convert to a batch of 1
+  xb = to_device(img.unsqueeze(0),device)
+  # Get predictions from model
+  yb = model(xb)
+  # Pick index with highest probability
+  prob,preds = torch.max(yb,dim = 1)
+  # Retrieve the class label
+  return dataset.classes[preds[0].item()]
+
+#
+
+import __main__
+setattr(__main__,"ResNet",ResNet)
+model_path = '/content/drive/MyDrive/NTUE 國北教大/專題/NTUE_Garbage_Classification/model.pt'
+model = torch.load(model_path)
+loaded_model = model
+
+#
+
+from PIL import Image
+from pathlib import Path
+
+def predict_external_image(image_name):
+  image = Image.open(Path('./'+image_name))
+
+  example_image = transformations(image)
+  plt.imshow(example_image.permute(1,2,0))
+  print("The image resembles",predict_image(example_image,loaded_model)+".")
+
+#
+
+predict_external_image('01.jpg')
