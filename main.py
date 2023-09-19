b665753c250ac927d631f09e501b1d35bc3b6a40
LeoYu <a18600083@gmail.com>
2023-09-19 15:00:08 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib
 import torch
 import torchvision
 from torch.utils.data import random_split
@@ -7,6 +8,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torchvision.models import ResNet
+import __main__
+from PIL import Image
+from pathlib import Path
+
+# %matplotlib inline
 
 data_dir = 'C:/Users/leozh/PycharmProjects/NTUE_Garbage_Classification/dataset'
 classes = os.listdir(data_dir)
@@ -15,113 +23,117 @@ print(classes)
 transformations = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
 dataset = ImageFolder(data_dir, transform=transformations)
 
-import matplotlib.pyplot as plt
-%matplotlib inline
 
-def show_sample(img,label):
-  print("label:",dataset.classes[label],"Class No:"+ str(label) + ")")
-  plt.imshow(img.permute(1,2,0))
+def show_sample(img, label):
+    print("label:", dataset.classes[label], "Class No:" + str(label) + ")")
+    plt.imshow(img.permute(1, 2, 0))
 
-  def accuracy(outputs, labels):
+
+def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
     return torch.tensor(torch.sum(preds == labels).item() / len(preds))
 
 
 class ImageClassificationBase(nn.Module):
-  def training_step(self, batch):
-    images, labels = batch
-    out = self(images)  # Generate predictions
-    loss = F.cross_entropy(out, labels)  # Calculate loss
-    return loss
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
 
-  def validation_step(self, batch):
-    images, labels = batch
-    out = self(images)  # Generate predictions
-    loss = F.cross_entropy(out, labels)  # Calculate loss
-    acc = accuracy(out, labels)  # Calculate accuracy
-    return {'val_loss': loss.detach(), 'val_acc': acc}
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
 
-  def validation_epoch_end(self, outputs):
-    batch_losses = [x['val_loss'] for x in outputs]
-    epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
-    batch_accs = [x['val_acc'] for x in outputs]
-    epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
-    return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
 
-  def epoch_end(self, epoch, result):
-    print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-      epoch + 1, result['train_loss'], result['val_loss'], result['val_acc']))
+    def epoch_end(self, epoch, result):
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch + 1, result['train_loss'], result['val_loss'], result['val_acc']))
 
-#
 
-  def get_default_device():
+class ResNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(pretrained=True)
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, len(dataset.classes))
+
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
+
+
+# porting gpu/cpu
+
+def get_default_device():
     """Pick GPU if available, else CPU"""
     if torch.cuda.is_available():
-      return torch.device('cuda')
+        return torch.device('cuda')
     else:
-      return torch.device('cpu')
+        return torch.device('cpu')
 
 
 def to_device(data, device):
-  """Move tensor(s) to chosen device"""
-  if isinstance(data, (list, tuple)):
-    return [to_device(x, device) for x in data]
-  return data.to(device, non_blocking=True)
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
 
 
 class DeviceDataLoader():
-  """Wrap a dataloader to move data to a device"""
+    """Wrap a dataloader to move data to a device"""
 
-  def __init__(self, dl, device):
-    self.dl = dl
-    self.device = device
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
 
-  def __iter__(self):
-    """Yield a batch of data after moving it to device"""
-    for b in self.dl:
-      yield to_device(b, self.device)
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
 
-  def __len__(self):
-    """Number of batches"""
-    return len(self.dl)
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
 
-#
 
 device = get_default_device()
-device
+print(device)
 
-#
 
-def predict_image(img,model):
-  # Convert to a batch of 1
-  xb = to_device(img.unsqueeze(0),device)
-  # Get predictions from model
-  yb = model(xb)
-  # Pick index with highest probability
-  prob,preds = torch.max(yb,dim = 1)
-  # Retrieve the class label
-  return dataset.classes[preds[0].item()]
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    prob, preds = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return dataset.classes[preds[0].item()]
 
-#
 
-import __main__
-setattr(__main__,"ResNet",ResNet)
-model_path = '/content/drive/MyDrive/NTUE 國北教大/專題/NTUE_Garbage_Classification/model.pt'
-model = torch.load(model_path)
+setattr(__main__, "ResNet", ResNet)
+model_path = 'C:/Users/leozh/PycharmProjects/NTUE_Garbage_Classification/model.pt'
+model = torch.load(model_path, map_location='cpu')
 loaded_model = model
 
-#
-
-from PIL import Image
-from pathlib import Path
 
 def predict_external_image(image_name):
-  image = Image.open(Path('./'+image_name))
+    image = Image.open(Path('./' + image_name))
 
-  example_image = transformations(image)
-  plt.imshow(example_image.permute(1,2,0))
-  print("The image resembles",predict_image(example_image,loaded_model)+".")
+    example_image = transformations(image)
+    plt.imshow(example_image.permute(1, 2, 0))
+    print("The image resembles", predict_image(example_image, loaded_model) + ".")
 
-#
 
 predict_external_image('01.jpg')
